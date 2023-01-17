0acdb58122caea3b6edf0c34ab825088017ca292
Ravhan Shafi <80163239+sayimshah@users.noreply.github.com>
2023-01-17 22:57:02 +0530

    com

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,7 +124,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     path('',include('ecomapp.urls')),
 
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
